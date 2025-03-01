03e7db5ca8be55ed21c4c2f0b5c09506136e9c16
alex81632 <alexandrebellargus@hotmail.com>
2025-03-01 20:17:05 -0300

    feat: explore problems page design

--- src/app/(home)/_components/layout/Header.tsx
+++ src/app/(home)/_components/layout/Header.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { LogIn } from "lucide-react";
 
 const Header = () => {
   return (
@@ -36,14 +37,10 @@ const Header = () => {
           ))}
         </nav>
         <Link href="/login" className="btn btn-primary">
-          <Image
-            src="/login.png"
-            alt="login"
-            width={50}
-            height={50}
-            priority
-            className="rounded-xl transform hover:scale-110 transition-transform duration-300"
-          />
+          <LogIn
+              size={24}
+              className="text-[--color-tacoyellow] transform hover:scale-110 transition-transform duration-300"
+            />
         </Link>
       </div>
     </header>
